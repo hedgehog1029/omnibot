3f097e60a7541bc753fa50263a19bc24eb7fa39d
hedgehog1029 <omgredstone@gmail.com>
2016-04-11 14:48:41 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// omnibot - a discord bot
+
+var discordie = require("discordie"),
+	fs = require("fs");
+
+var commandeer = require("./lib/commandeer.js"),
+	commands = require("./lib/commands.js"),
+	mods = require("./lib/mods.js");
+
+console.log("Starting OmniBot v0.1")
+
+var config = JSON.parse(fs.readFileSync(__dirname + "/config/config.json", 'utf8'));
+var bot = new discordie();
+
+bot.connect({
+	token: config.token
+});
+
+bot.Dispatcher.on(discordie.Events.GATEWAY_READY, function(e) {
+	console.log("OmniBot started. Username: " + bot.User.username);
+
+	global.InviteManager = bot.Invites;
+	global.bot_id = bot.User.id;
+	//bot.User.edit(null, null, fs.readFileSync("profile.png")); // temp profile update
+});
+
+bot.Dispatcher.on(discordie.Events.MESSAGE_CREATE, function(e) {
+	commandeer.dispatch(e.message);
+});
